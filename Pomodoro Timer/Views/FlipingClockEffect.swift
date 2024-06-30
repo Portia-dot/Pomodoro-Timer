881f96045ebfc708aaffe4b295963f03d5f22397
Oluwayomi Felix <77457879+Portia-dot@users.noreply.github.com>
2024-06-30 17:25:41 -0600

    update

--- Pomodoro Timer/Views/FlipingClockEffect.swift
+++ Pomodoro Timer/Views/FlipingClockEffect.swift
@@ -26,65 +26,72 @@ struct FlipingClockEffect: View {
         ZStack{
             UnevenRoundedRectangle(topLeadingRadius: cornerRadius, bottomLeadingRadius: 0, bottomTrailingRadius: 0, topTrailingRadius: cornerRadius)
                 .fill(background.shadow(.inner(radius: 1)))
-                .overlay(alignment: .top){
+                .frame(height: halfHeight)
+                .overlay(alignment: .top) {
+                    TextView(nextValue)
+                        .frame(width: size.width, height: size.height)
+                        .drawingGroup()
+                }
+                .clipped()
+                .frame(maxHeight: .infinity, alignment: .top)
+            
+            UnevenRoundedRectangle(topLeadingRadius: cornerRadius, bottomLeadingRadius: 0, bottomTrailingRadius: 0, topTrailingRadius: cornerRadius)
+                .fill(background.shadow(.inner(radius: 1)))
+                .frame(height: halfHeight)
+                .modifier(
+                    RotationModifider(
+                        rotation: rotation,
+                        currentValue: currentValue,
+                        nextValue: nextValue,
+                        fontSize: fontSize,
+                        foreground: foreground,
+                        size: size
+                    )
+                )
+                .clipped()
+                .rotation3DEffect(
+                    .init(degrees: rotation),
+                    axis: (x: 1.0, y: 0.0, z: 0.0),
+                    anchor: .bottom,
+                    anchorZ: 0.01,
+                    perspective: 0.4
+                )
+                .frame(maxHeight: .infinity, alignment: .top)
+                .zIndex(10)
+            
+            UnevenRoundedRectangle(topLeadingRadius: 0, bottomLeadingRadius: cornerRadius, bottomTrailingRadius: cornerRadius, topTrailingRadius: 0)
+                .fill(background.shadow(.inner(radius: 1)))
+                .frame(height: halfHeight)
+                .overlay(alignment: .bottom) {
                     TextView(currentValue)
                         .frame(width: size.width, height: size.height)
                         .drawingGroup()
                 }
                 .clipped()
-                .frame(maxWidth: .infinity, alignment: .top)
-            UnevenRoundedRectangle(topLeadingRadius: cornerRadius, bottomLeadingRadius: 0, bottomTrailingRadius: 0, topTrailingRadius: cornerRadius)
-                .fill(background.shadow(.inner(radius: 1)))
-                .frame(height: halfHeight)
-                .modifier(
-                RotationModifider(rotation: rotation, currentValue: currentValue, nextValue: nextValue, fontSize: fontSize, foreground: foreground, size: size)
-                )
-                .clipped()
-                .rotation3DEffect(
-                    .init(degrees: rotation),
-                    axis: (x: 1.0, y: 0.0, z: 0.0),
-                    anchor: .bottom,
-                    anchorZ: 0.01,
-                    perspective: 0.4
-                )
-                .frame(maxHeight: .infinity, alignment: .top)
-                .zIndex(10)
-            UnevenRoundedRectangle(topLeadingRadius: cornerRadius, bottomLeadingRadius: 0, bottomTrailingRadius: 0, topTrailingRadius: cornerRadius)
-                .fill(background.shadow(.inner(radius: 1)))
-                .frame(height: halfHeight)
-                .modifier(
-                RotationModifider(rotation: rotation, currentValue: currentValue, nextValue: nextValue, fontSize: fontSize, foreground: foreground, size: size)
-                )
-                .clipped()
-                .rotation3DEffect(
-                    .init(degrees: rotation),
-                    axis: (x: 1.0, y: 0.0, z: 0.0),
-                    anchor: .bottom,
-                    anchorZ: 0.01,
-                    perspective: 0.4
-                )
-                .frame(maxHeight: .infinity, alignment: .top)
-                .zIndex(10)
+                .frame(maxHeight: .infinity, alignment: .bottom)
         }
         .frame(width: size.width, height: size.height)
         .onChange(of: value, initial: true) { oldValue, newValue in
             currentValue = oldValue
             nextValue = newValue
             
-            guard rotation == 0 else{
+            guard rotation == 0 else {
                 currentValue = newValue
                 return
             }
+            
             guard oldValue != newValue else { return }
             
-            withAnimation(.easeInOut(duration: animationDuration), completionCriteria: .logicallyComplete){
+            withAnimation(.easeInOut(duration: animationDuration), completionCriteria: .logicallyComplete) {
                 rotation = -180
-            }completion: {
+            } completion: {
                 rotation = 0
-                currentValue =  value
+                currentValue = value
             }
         }
     }
+    
+    
     @ViewBuilder
     func TextView(_ value: Int) -> some View {
         Text("\(value)")
@@ -108,10 +115,30 @@ fileprivate struct RotationModifider: ViewModifier, Animatable {
     }
     func body(content: Content) -> some View {
         content
+            .overlay(alignment: .top){
+                Group {
+                    if -rotation > 90 {
+                        Text("\(nextValue)")
+                            .font(.system(size: fontSize).bold())
+                            .foregroundStyle(foreground)
+                            .scaleEffect(x: 1, y: -1)
+                            .transition(.identity)
+                            .lineLimit(1)
+                    }else{
+                        Text("\(currentValue)")
+                            .font(.system(size: fontSize).bold())
+                            .foregroundStyle(foreground)
+                            .transition(.identity)
+                            .lineLimit(1)
+                    }
+                }
+            }
+            .frame(width: size.width, height: size.height)
+            .drawingGroup()
         
     }
 }
 
 #Preview {
-   ContentView()
+    ContentView()
 }
