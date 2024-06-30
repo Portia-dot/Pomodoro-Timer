11b0f6d27f2ed7c437b3c9deaf24c88ef447c4fe
Oluwayomi Felix <77457879+Portia-dot@users.noreply.github.com>
2024-06-29 22:55:27 -0600

    Initial Commit

---
+++ Pomodoro Timer/Pomodoro_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pomodoro_TimerApp.swift
+//  Pomodoro Timer
+//
+//  Created by Modamori Oluwayomi on 2024-06-29.
+//
+
+import SwiftUI
+
+@main
+struct Pomodoro_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
